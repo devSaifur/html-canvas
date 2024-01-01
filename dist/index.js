52d65d6410e4368ad85d4f2041d7a456b4e48926
Saifur Rahman <saifur2akash@gmail.com>
2024-01-02 02:54:34 +0600

    bla bla bla

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const particlesArray = [];
+let hue = 0;
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -11,9 +12,11 @@ canvas.height = window.innerHeight;
 const mouse = { x: -1000, y: -1000 };
 canvas.addEventListener('click', () => {
     canvas.addEventListener('mousemove', (e) => {
-        mouse.x = e.clientX;
-        mouse.y = e.clientY;
-        drawCircle(mouse.x, mouse.y, 15);
+        mouse.x = e.x;
+        mouse.y = e.y;
+        for (let i = 0; i < 5; i++) {
+            particlesArray.push(new Particle());
+        }
     });
 });
 function drawCircle(mouseX, mouseY, radius) {
@@ -24,38 +27,59 @@ function drawCircle(mouseX, mouseY, radius) {
 }
 class Particle {
     constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = Math.random() * canvas.width
+        // this.y = Math.random() * canvas.height
+        this.size = Math.random() * 5 + 10;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
+        this.color = `hsl(${hue}, 100%, 50%)`;
     }
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.2)
+            this.size -= 0.1;
     }
     draw() {
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 25, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
     }
 }
-function init() {
-    for (let i = 0; i < 100; i++) {
-        particlesArray.push(new Particle());
-    }
-}
-init();
 function handleParticles() {
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
+        //this I don't understand lol
+        for (let j = i; j < particlesArray.length; j++) {
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].x;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < 100) {
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWidth = 0.1;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+        if (particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1);
+            i--;
+        }
     }
 }
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = 'rgba(0,0,0,0.02)'
+    // ctx.fillRect(0, 0, canvas.width, canvas.height)
     handleParticles();
+    hue += 0.5; // how fast the color changes
     requestAnimationFrame(animate);
 }
 animate();
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ const canvas = document.querySelector('canvas') as HTMLCanvasElement
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 
 const particlesArray: Particle[] = []
+let hue = 0
 
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth
@@ -15,9 +16,12 @@ const mouse = { x: -1000, y: -1000 }
 
 canvas.addEventListener('click', () => {
   canvas.addEventListener('mousemove', (e) => {
-    mouse.x = e.clientX
-    mouse.y = e.clientY
-    drawCircle(mouse.x, mouse.y, 15)
+    mouse.x = e.x
+    mouse.y = e.y
+
+    for (let i = 0; i < 5; i++) {
+      particlesArray.push(new Particle())
+    }
   })
 })
 
@@ -34,42 +38,63 @@ class Particle {
   size: number
   speedX: number
   speedY: number
+  color: string
   constructor() {
-    this.x = Math.random() * canvas.width
-    this.y = Math.random() * canvas.height
-    this.size = Math.random() * 5 + 1
+    this.x = mouse.x
+    this.y = mouse.y
+    // this.x = Math.random() * canvas.width
+    // this.y = Math.random() * canvas.height
+    this.size = Math.random() * 5 + 10
     this.speedX = Math.random() * 3 - 1.5
     this.speedY = Math.random() * 3 - 1.5
+    this.color = `hsl(${hue}, 100%, 50%)`
   }
   update() {
     this.x += this.speedX
     this.y += this.speedY
+    if (this.size > 0.2) this.size -= 0.1
   }
   draw() {
-    ctx.fillStyle = 'blue'
+    ctx.fillStyle = this.color
     ctx.beginPath()
-    ctx.arc(this.x, this.y, 25, 0, Math.PI * 2)
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
     ctx.fill()
   }
 }
-
-function init() {
-  for (let i = 0; i < 100; i++) {
-    particlesArray.push(new Particle())
-  }
-}
-init()
 
 function handleParticles() {
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update()
     particlesArray[i].draw()
+
+    //this I don't understand lol
+    for (let j = i; j < particlesArray.length; j++) {
+      const dx = particlesArray[i].x - particlesArray[j].x
+      const dy = particlesArray[i].y - particlesArray[j].x
+      const distance = Math.sqrt(dx * dx + dy * dy)
+      if (distance < 100) {
+        ctx.beginPath()
+        ctx.strokeStyle = particlesArray[i].color
+        ctx.lineWidth = 0.1
+        ctx.moveTo(particlesArray[i].x, particlesArray[i].y)
+        ctx.lineTo(particlesArray[j].x, particlesArray[j].y)
+        ctx.stroke()
+        ctx.closePath()
+      }
+    }
+    if (particlesArray[i].size <= 0.3) {
+      particlesArray.splice(i, 1)
+      i--
+    }
   }
 }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  // ctx.fillStyle = 'rgba(0,0,0,0.02)'
+  // ctx.fillRect(0, 0, canvas.width, canvas.height)
   handleParticles()
+  hue += 0.5 // how fast the color changes
   requestAnimationFrame(animate)
 }
 
